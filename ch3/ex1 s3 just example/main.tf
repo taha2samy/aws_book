e227330379d502b35105a7b949f4ff372d97da01
Your Name <samyt8435@gmail.com>
2025-07-15 15:02:50 +0300

    tes

--- ch3/ex1 s3 just example/main.tf
+++ ch3/ex1 s3 just example/main.tf
@@ -1,13 +1,12 @@
 resource "aws_s3_bucket" "my_s3_demo_bucket" {
   bucket = "my-unique-s3-flat-demo-bucket-123456789" 
-  force_destroy = true # Allows deletion of the bucket even if it contains objects
+  force_destroy = true # Allows deletion of non-empty bucket
 }
 resource "aws_s3_object" "just_a_file" {
   bucket = aws_s3_bucket.my_s3_demo_bucket.id
   key    = "just_a_file.txt"
   source = "${path.root}/just_a_file.txt"
   content_type = "text/plain"
-  
 }
 resource "aws_s3_bucket_object" "just_a_content" {
   bucket = aws_s3_bucket.my_s3_demo_bucket.id
